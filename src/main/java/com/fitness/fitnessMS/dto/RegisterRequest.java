6f0c50440a66bda0a729cb27e6a141e21d354bed
Devendune <devendunegi06@gmail.com>
2026-03-04 11:59:58 +0530

    added name to the input dto

--- src/main/java/com/fitness/fitnessMS/dto/RegisterRequest.java
+++ src/main/java/com/fitness/fitnessMS/dto/RegisterRequest.java
@@ -7,6 +7,9 @@ import lombok.Data;
 @Data
 public class RegisterRequest
 {
+    @NotBlank(message="Name is required")
+    private String name;
+
     @NotBlank(message="Email is required")
     private String email;
 
